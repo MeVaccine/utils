afe994642f65875e6731cd2323dedbc5b801a3ce
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2021-06-11 21:55:34 +0700

    ADD vaccine name back

--- bangkok-hospital/index.js
+++ bangkok-hospital/index.js
@@ -1,59 +1,53 @@
-const fs = require('fs')
-const path = require('path')
-const generateDateTime = require('./dateTime')
-const nonthaburi = require('./nonthaburi.json')
-const bangkok = require('./bangkok.json')
+const fs = require("fs")
+const path = require("path")
+const generateDateTime = require("./dateTime")
+const nonthaburi = require("./nonthaburi.json")
+const bangkok = require("./bangkok.json")
 
 const avaliableVaccines = [
 	{
-		vaccine: {
-			$oid: '60c1872339fa5e71b0ca2fad',
-		},
+		name: "Sinovac",
 		amount: 5000,
-		avaliable: 5000,
+		avaliable: 5000
 	},
 	{
-		vaccine: {
-			$oid: '60c1872339fa5e71b0ca2fae',
-		},
+		name: "Sinopharm",
 		amount: 5000,
-		avaliable: 5000,
+		avaliable: 5000
 	},
 	{
-		vaccine: {
-			$oid: '60c1872339fa5e71b0ca2faf',
-		},
+		name: "Oxford–AstraZeneca",
 		amount: 10000,
-		avaliable: 10000,
+		avaliable: 10000
 	},
 	{
-		vaccine: {
-			$oid: '60c1872339fa5e71b0ca2fb0',
-		},
+		name: "Pfizer-BioNTech",
 		amount: 10000,
-		avaliable: 10000,
+		avaliable: 10000
 	},
 	{
-		vaccine: {
-			$oid: '60c1872339fa5e71b0ca2fb1',
-		},
+		name: "Moderna",
 		amount: 5000,
-		avaliable: 5000,
-	},
+		avaliable: 5000
+	}
 ]
 
 const generateData = (locations, province_th, province_en) => {
-	return locations.map(el => {
+	return locations.map((el) => {
 		const generatedDateTime = generateDateTime()
 		const vaccineRandom = Math.floor(Math.random() * 3)
 		const data = {
 			name_th: el.name_th,
 			name_en: el.name_en,
 			dateTime: generatedDateTime,
-			vaccines: [avaliableVaccines[0], avaliableVaccines[1], avaliableVaccines[2]],
+			vaccines: [
+				avaliableVaccines[0],
+				avaliableVaccines[1],
+				avaliableVaccines[2]
+			],
 			priority: el.priority,
 			province_th,
-			province_en,
+			province_en
 		}
 
 		for (let i = 1; i <= vaccineRandom; i++) {
@@ -64,9 +58,12 @@ const generateData = (locations, province_th, province_en) => {
 }
 
 const allData = [
-	...generateData(bangkok, 'กรุงเทพมหานคร', 'Bangkok'),
-	...generateData(nonthaburi, 'นนทบุรี', 'Nonthaburi'),
+	...generateData(bangkok, "กรุงเทพมหานคร", "Bangkok"),
+	...generateData(nonthaburi, "นนทบุรี", "Nonthaburi")
 ]
 
-fs.unlinkSync(path.resolve(__dirname, 'locations.json'))
-fs.writeFileSync(path.resolve(__dirname, 'locations.json'), JSON.stringify(allData))
+fs.unlinkSync(path.resolve(__dirname, "locations.json"))
+fs.writeFileSync(
+	path.resolve(__dirname, "locations.json"),
+	JSON.stringify(allData)
+)
