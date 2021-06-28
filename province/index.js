357dd73911158ba5191afc274939d48ad594362d
Sethanant Pipatpakorn <sethanant.p@icloud.com>
2021-06-28 11:18:27 +0700

    ADD eng

--- province/index.js
+++ province/index.js
@@ -6,8 +6,10 @@ const textData = fs.readFileSync(path.resolve(__dirname, 'data.txt'), 'utf8')
 const provinceTextArray = textData.split('\n')
 
 const provinceArray = provinceTextArray.map(el => {
-	const regexArray = /.*'([ก-๛]*)'.*/gu.exec(el)
-	return regexArray[1]
+	const regexArray = /.*'([ก-๛]*)', '(.*)'.*/gu.exec(el)
+	return { TH: regexArray[1], EN: regexArray[2] }
 })
 
-console.log(provinceArray)
+// console.log(JSON.stringify(provinceArray))
+
+fs.writeFileSync(path.resolve(__dirname, 'province.json'), JSON.stringify(provinceArray))
